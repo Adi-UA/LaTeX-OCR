a0121809e20a99e8f8d2d75dad6cfa1eb42d7741
Adi-UA <abanerjee.email@gmail.com>
2023-10-04 11:58:25 -0700

    Actually setting shuffle seed

--- scripts/shuffle_data.py
+++ scripts/shuffle_data.py
@@ -1,25 +1,27 @@
+import argparse
 import os
 import random
 import shutil
-import argparse
+
 import yaml
 
 # Define source and destination directories
 project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-images_dir = os.path.join(project_dir, 'dataset/data/images')
-dest_dir = os.path.join(project_dir, 'dataset/data/valimages')
+images_dir = os.path.join(project_dir, "dataset/data/images")
+dest_dir = os.path.join(project_dir, "dataset/data/valimages")
 
 # Parse command-line arguments to get the config file path
 parser = argparse.ArgumentParser()
-parser.add_argument('--config', type=str, default='custom/og_config.yaml')
+parser.add_argument("--config", type=str, default="custom/og_config.yaml")
 args = parser.parse_args()
 config_file = os.path.join(project_dir, args.config)
 
 # Load seed value from the config file
-with open(config_file, 'r') as f:
+with open(config_file, "r") as f:
     params = yaml.load(f, Loader=yaml.FullLoader)
 
-seed = params.get('seed', 42)  # Use 42 as the default seed if not specified
+seed = params.get("seed", 42)  # Use 42 as the default seed if not specified
+random.seed(seed)
 
 # List all files in the source directory
 all_items = os.listdir(images_dir)
